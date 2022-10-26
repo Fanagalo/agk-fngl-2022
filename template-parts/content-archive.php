b175159290ebc6d74c8df888f3c6fd1aeccd460b
Jaap Wiering <github.com@fanagalo.nl>
2022-10-26 17:06:45 +0200

    archive pages excerpts and styling

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -11,9 +11,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+
+
 	<?php agk_fngl_2022_post_thumbnail(); ?>
 
 	<header class="article-header">
+
 
 		<?php
 		the_title('<h2 class="article-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
@@ -29,27 +32,7 @@
         </header><!-- .article-header -->
 
 	<div class="article-content">
-		<?php
-            the_excerpt();
-
-        // the_content(sprintf(
-		// 	wp_kses(
-		// 		/* translators: %s: Name of current post. Only visible to screen readers */
-		// 		__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'agk-fngl-2022'),
-		// 		array(
-		// 			'span' => array(
-		// 				'class' => array(),
-		// 			),
-		// 		)
-		// 	),
-		// 	get_the_title()
-		// ));
-
-		// wp_link_pages(array(
-		// 	'before' => '<div class="page-links">' . esc_html__('Pages:', 'agk-fngl-2022'),
-		// 	'after'  => '</div>',
-		// ));
-		?>
+		<?php echo '<p>' . wpse_custom_excerpts(55). '</p>'; ?>
 	</div><!-- .article-content -->
 
 
