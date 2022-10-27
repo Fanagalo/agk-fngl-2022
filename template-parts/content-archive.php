7b0d9cc534fc5a37335323761d8707721f105380
Jaap Wiering <github.com@fanagalo.nl>
2022-10-27 15:24:18 +0200

    archive layout

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -11,25 +11,21 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-
-
 	<?php agk_fngl_2022_post_thumbnail(); ?>
 
 	<header class="article-header">
-
-
-		<?php
-		the_title('<h2 class="article-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-		?>
-			<div class="article-meta">
-				<?php
+		<div class="article-meta">
+			<?php
 				/* alternative to the_date(); prints dates, also if previous article has same date */
 				$article_date = get_the_date();
 				echo '<span class="post-date">' . $article_date . '</span>';
-				?>
-			</div><!-- .article-meta -->
+			?>
+		</div><!-- .article-meta -->
 
-        </header><!-- .article-header -->
+		<?php
+			the_title('<h2 class="article-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+		?>
+    </header><!-- .article-header -->
 
 	<div class="article-content">
 		<?php echo '<p>' . wpse_custom_excerpts(45). '</p>'; ?>
