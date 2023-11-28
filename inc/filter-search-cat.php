b628672bbc98f671702aba9dd1c4267ceda950c3
Jaap Wiering <github.com@fanagalo.nl>
2023-11-28 14:28:24 +0100

    add pages to search query

--- inc/filter-search-cat.php
+++ inc/filter-search-cat.php
@@ -2,7 +2,7 @@
 
 function agk_fngl_2022_filter_search_cat($query) {
     if ($query->is_search && !is_admin() ) {
-        $query->set('post_type','post');
+        $query->set('post_type', array('post', 'page'));
         $query->set('cat',array('201','203','214','215'));
     }
 return $query;
